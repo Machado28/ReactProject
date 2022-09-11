eb647fa6358078e5b3aaa5363a801f233178a55e
Antonio <ulundoantonio@gmail.com>
2022-09-11 16:32:15 +0100

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports={
+ entry:path.resolve(__dirname,'src','index.js'),
+ output:{
+  path:path.resolve(__dirname,'public'),
+  filename:'bundle.js'
+ },
+ devServer:{
+ 
+    static: path.resolve(__dirname, 'public'),
+    port: 8080,
+    open: true,
+    hot: true
+},
+ 
+ module:{
+   rules:[
+     {
+       test:/\.js$/,
+       exclude:/node_modules/,
+       use:{
+         loader:'babel-loader'
+       }
+     }
+   ]
+ }
+}
